ac7594b5e705ea846852eb2a248e47395d4ed6f0
lesedow <lesedow@gmail.com>
2023-09-19 18:32:58 +0300

    Limit number size
    Prevent both numbers of the operation
    to have a length bigger than 9.

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
-function Calculator() {
-    this.operations = {
-        '+': (a, b) => a + b,
-        '-': (a, b) => a - b,
-        '*': (a, b) => a * b,
-        '/': (a, b) => a / b
-    }
-    this.evaluate = expression => {
-        const [a, operator, b] = expression.split(' ');
-        const evaluation = this.operations[operator](+a, +b);
-        return evaluation;
+import { Calculator } from "./calculator.js";
+
+const display = document.querySelector('.display');
+const calculatorButtons = document.querySelector('.calculator-buttons');
+
+let firstNumber = 0;
+let secondNumber = 0;
+let currentOperator = null;
+
+const buttonsFunction = {
+    number: setNumber,
+    operator: setOperator,
+    clear: 1,
+    sign: 1,
+    percent: 1,
+    comma: 1
+}
+
+function updateDisplay (number) {
+    display.textContent = number.toLocaleString();
+}
+
+function setOperator (operator) {
+    currentOperator = operator;
+}
+
+function isNumberAtLimit (number) {
+    return number.toString().length === 9;
+}
+
+function setNumber (number) {
+    if (!currentOperator) {
+        if (isNumberAtLimit(firstNumber)) return
+        firstNumber = parseInt(`${firstNumber}${number}`);
+        updateDisplay(firstNumber);
+    } else {
+        if (isNumberAtLimit(secondNumber)) return
+        secondNumber = parseInt(`${secondNumber}${number}`);
+        updateDisplay(secondNumber);
     }
 }
 
-const calculator = new Calculator();
-const result = calculator.evaluate('2 * 3');
-console.log(result)
+calculatorButtons.addEventListener('click', (event) => {
+    const buttonValue = event.target.getAttribute('data-button');
+    if(!buttonValue) return;
+
+    switch (buttonValue) {
+        case 'number':
+            buttonsFunction.number(event.target.textContent);
+            break;
+        case 'operator':
+            buttonsFunction.operator(event.target.textContent);
+            break;
+    }
+})
