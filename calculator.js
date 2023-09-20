3f027ec199d6fcf18028dbb60981327c107c94c2
lesedow <lesedow@gmail.com>
2023-09-20 18:31:21 +0300

    Finished project

--- calculator.js
+++ calculator.js
@@ -1,17 +1,28 @@
 export function Calculator() {
+    this.firstNumber = 0;
+    this.secondNumber = null;
+    this.operator = null;
     this.operations = {
         '+': (a, b) => a + b,
         '-': (a, b) => a - b,
         '*': (a, b) => a * b,
         '/': (a, b) => a / b
     }
-    this.evaluate = expression => {
-        const [a, operator, b] = expression.split(' ');
-        const evaluation = this.operations[operator](+a, +b);
+    this.evaluate = () => {
+        const evaluation = this.operations[this.operator](this.firstNumber, this.secondNumber);
+        this.firstNumber = evaluation;
+        this.secondNumber = null;
+        this.operator = null;
         return evaluation;
     }
+    this.setOperator = (operator) => {
+        this.operator = operator;
+    }
+    this.isOperator = () => this.operator;
+    this.setFirstNumber = (number) => {
+        this.firstNumber = number;
+    }
+    this.setSecondNumber = (number) => {
+        this.secondNumber = number;
+    }
 }
-
-const calculator = new Calculator();
-const result = calculator.evaluate('2 * 3');
-console.log(result)
--- index.js
+++ index.js
@@ -2,54 +2,121 @@ import { Calculator } from "./calculator.js";
 
 const display = document.querySelector('.display');
 const calculatorButtons = document.querySelector('.calculator-buttons');
+const calculator = new Calculator();
 
-let firstNumber = 0;
-let secondNumber = 0;
-let currentOperator = null;
-
-const buttonsFunction = {
-    number: setNumber,
-    operator: setOperator,
-    clear: 1,
-    sign: 1,
-    percent: 1,
-    comma: 1
-}
+let operatorSelected = false;
 
 function updateDisplay (number) {
-    display.textContent = number.toLocaleString();
+    if (
+        isDisplayLimit() && 
+        !operatorSelected
+    ) return;
+
+    if (operatorSelected) {
+        calculator.setFirstNumber(formatNumber(display.textContent));
+        display.textContent = '';
+        operatorSelected = false;
+    }
+
+    display.textContent += number;
+    display.textContent = convertToLocaleString(display.textContent);
 }
 
-function setOperator (operator) {
-    currentOperator = operator;
-}
+function displayResult () {
+    if (calculator.isOperator()) {
+        calculator.setSecondNumber(formatNumber(display.textContent));
+        let result = calculator.evaluate();
+        if (
+            result.toString().includes('.') &&
+            !result.toString().includes('e')
+        ) {
+            const resultSplitted = result.toString().split('.');
+            if (resultSplitted[0].length <= 9) {
+                result = result.toFixed(4)
+            } else {
+                result = result.toExponential(4);
+            }
 
-function isNumberAtLimit (number) {
-    return number.toString().length === 9;
-}
-
-function setNumber (number) {
-    if (!currentOperator) {
-        if (isNumberAtLimit(firstNumber)) return
-        firstNumber = parseInt(`${firstNumber}${number}`);
-        updateDisplay(firstNumber);
-    } else {
-        if (isNumberAtLimit(secondNumber)) return
-        secondNumber = parseInt(`${secondNumber}${number}`);
-        updateDisplay(secondNumber);
+        } else if (result.toString().length > 9) {
+            result = result.toExponential(4)
+        }
+        display.textContent = convertToLocaleString(result.toString())
     }
 }
 
-calculatorButtons.addEventListener('click', (event) => {
-    const buttonValue = event.target.getAttribute('data-button');
-    if(!buttonValue) return;
+function changeSign () {
+    if (display.textContent.startsWith('-')) {
+        display.textContent = display.textContent.slice(1);
+    } else {
+        display.textContent = `-${display.textContent}`;
+    }
+}
 
-    switch (buttonValue) {
+function convertToLocaleString(value) {
+    if (value.includes('.')) {
+        const [firstPart, secondPart] = value.split('.');
+        return `${firstPart.toLocaleString()}.${secondPart}`;
+    }
+    return parseFloat(value.split(',').join('')).toLocaleString();
+}
+
+function formatNumber (string) {
+    return parseFloat(string.split(',').join(''));
+}
+
+function setOperator (operator) {
+    if (calculator.isOperator()) return;
+    calculator.setOperator(operator);
+    operatorSelected = true;
+}
+
+function isDisplayLimit () {
+    const displayFormatted = display.textContent.split(/[^0-9]/g).join('');
+    return displayFormatted.length >= 9;
+}
+
+function clearDisplay () {
+    operatorSelected = false;
+    display.textContent = '';
+    calculator.setFirstNumber(0);
+    calculator.setSecondNumber(null);
+    calculator.setOperator(null);
+}
+
+function addDecimal () {
+    if (
+        display.textContent.includes('.') ||
+        display.textContent.length === 0
+    ) return;
+
+    display.textContent += '.';
+}
+
+calculatorButtons.addEventListener('click', (event) => {
+    const button = event.target.getAttribute('data-button');
+    if(!button) return;
+
+    const buttonValue = event.target.textContent;
+
+    switch (button) {
         case 'number':
-            buttonsFunction.number(event.target.textContent);
+            updateDisplay(buttonValue);
             break;
         case 'operator':
-            buttonsFunction.operator(event.target.textContent);
+            setOperator(buttonValue);
             break;
+        case 'equals':
+            displayResult();
+            break;
+        case 'sign':
+            changeSign();
+            break;
+        case 'clear':
+            clearDisplay();
+            break;
+        case 'decimal':
+            addDecimal();
+            break;
+
     }
 })
